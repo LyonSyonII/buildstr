20f2969b55e03dc6d93c61c28be1e8fe3ffe9e0c
lyonsyonii <liam@garriga.dev>
2023-09-06 16:10:55 +0200

    std::ffi::FromBytesWithNulError

--- buildstr/src/_std/ffi.rs
+++ buildstr/src/_std/ffi.rs
@@ -2,21 +2,36 @@ use crate::BuildStr;
 
 impl BuildStr for ::core::ffi::CStr {
     fn to_build_string(&self) -> String {
-        format!("::core::ffi::CStr::from_bytes_with_nul({}).unwrap()", self.to_bytes_with_nul().to_build_string())
+        format!("::std::ffi::CStr::from_bytes_with_nul({}).unwrap()", self.to_bytes_with_nul().to_build_string())
     }
 }
+
 impl BuildStr for ::std::ffi::CString {
     fn to_build_string(&self) -> String {
-        format!("::core::ffi::CString::new({}).unwrap()", self.to_bytes_with_nul().to_build_string())
+        format!("::std::ffi::CString::new({}).unwrap()", self.to_bytes_with_nul().to_build_string())
     }
 }
+
 impl BuildStr for &::std::ffi::OsStr {
     fn to_build_string(&self) -> String {
-        format!("::core::ffi::OsStr::new({self:?})")
+        format!("::std::ffi::OsStr::new({self:?})")
     }
 }
+
 impl BuildStr for ::std::ffi::OsString {
     fn to_build_string(&self) -> String {
-        format!("::core::ffi::OsString::from({self:?})")
+        format!("::std::ffi::OsString::from({self:?})")
+    }
+}
+
+impl BuildStr for std::ffi::FromBytesWithNulError {
+    fn to_build_string(&self) -> String {
+        let s = self.to_string();
+        let s = match s.as_str() {
+            "data provided contains an interior nul byte" => "\0a",
+            "data provided is not nul terminated" => "",
+            _ => todo!("{s:?} case is not handled. Please, open an issue at https://github.com/lyonsyonii/buildstr.")
+        };
+        format!("::std::ffi::CStr::from_bytes_with_nul(\"{s}\").unwrap_err()")
     }
 }
