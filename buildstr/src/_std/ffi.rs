94c317fbd56ebf592e09fb772913e56638e1da72
lyonsyonii <liam@garriga.dev>
2023-09-07 19:01:40 +0200

    Updated test actions to only execute on pull request

--- buildstr/src/_std/ffi.rs
+++ buildstr/src/_std/ffi.rs
@@ -65,7 +65,7 @@ impl BuildStr for std::ffi::NulError {
 impl BuildStr for std::ffi::c_void {
     fn to_build_string(&self) -> String {
         // SAFETY: std::ffi::c_void is repr(u8)
-        let n: u8 = unsafe{std::mem::transmute_copy(self) };
+        let n: u8 = unsafe { std::mem::transmute_copy(self) };
         format!("unsafe{{std::mem::transmute::<u8,::std::ffi::c_void>({n})}}")
     }
 }
